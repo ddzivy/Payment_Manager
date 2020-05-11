632eacaa6e6f6d14c5a6b6ab1795a84c7aac975d
Daniel Dzivý <60179342+ddzivy@users.noreply.github.com>
2020-05-11 17:09:21 +0300

    final

--- src/main/java/com/example/P_Manager/PManagerApplication.java
+++ src/main/java/com/example/P_Manager/PManagerApplication.java
@@ -23,34 +23,36 @@ public class PManagerApplication {
 		SpringApplication.run(PManagerApplication.class, args);
 	}
 	
-	@Bean
-	public CommandLineRunner paymentDemo(PaymentRepository prepository, TypeRepository trepository, PersonRepository urepository) {
-		return (args) -> {
-			log.info("save a couple of payments");
-			
-			// Creating and saving new payment types
-			trepository.save(new Type("Product"));
-			trepository.save(new Type("Service"));
-			trepository.save(new Type("Refund")); 
-			
-			// Creating and saving new payments
-			prepository.save(new Payment("Table.oy", "PM0009874321", "245€", "2020-06-01", trepository.findByName("Product").get(0), "No"));
-			prepository.save(new Payment("Peter Smith", "PM0005879454", "2500€", "2020-05-27", trepository.findByName("Service").get(0), "No"));
-			prepository.save(new Payment("Mary West", "PM0045200148", "750€", "2020-05-10", trepository.findByName("Refund").get(0), "No"));	
-			
-			// Creating and saving new persons (user/password): secretary/secretary manager/manager boss/boss
-			Person user1 = new Person("secretary", "$2a$10$CG7xagtTGnmbd85JWF1IZeLRAGnbUJ1M9TNX2SyRQkzjkrh1wI1RG", "secretary@pm.oy", "USER");
-			Person user2 = new Person("manager", "$2a$10$5V7IGDsCaCx6tFzaz84wWu5StIlDdGyMYaW73MHt59M/3JFE6s1vG", "manager@pm.oy", "ADMIN");
-			Person user3 = new Person("boss","$2a$10$IxIjuRzI/k6epKZ9cN6bIuFTAVG1IlyWg8OgvJlL/d3sUQaxEcPyy", "boss@pm.oy", "ADMIN");
-			urepository.save(user1);
-			urepository.save(user2);
-			urepository.save(user3);
-			
-			log.info("fetch all payments");
-			for (Payment payment : prepository.findAll()) {
-				log.info(payment.toString());
-			}
-
-		};
-	}
+	/*
+	 * @Bean public CommandLineRunner paymentDemo(PaymentRepository prepository,
+	 * TypeRepository trepository, PersonRepository urepository) { return (args) ->
+	 * { log.info("save a couple of payments");
+	 * 
+	 * // Creating and saving new payment types trepository.save(new
+	 * Type("Product")); trepository.save(new Type("Service")); trepository.save(new
+	 * Type("Refund"));
+	 * 
+	 * // Creating and saving new payments prepository.save(new Payment("Table.oy",
+	 * "PM0009874321", "245€", "2020-06-01",
+	 * trepository.findByName("Product").get(0), "No")); prepository.save(new
+	 * Payment("Peter Smith", "PM0005879454", "2500€", "2020-05-27",
+	 * trepository.findByName("Service").get(0), "No")); prepository.save(new
+	 * Payment("Mary West", "PM0045200148", "750€", "2020-05-10",
+	 * trepository.findByName("Refund").get(0), "No"));
+	 * 
+	 * // Creating and saving new persons (user/password): secretary/secretary
+	 * manager/manager boss/boss Person user1 = new Person("secretary",
+	 * "$2a$10$CG7xagtTGnmbd85JWF1IZeLRAGnbUJ1M9TNX2SyRQkzjkrh1wI1RG",
+	 * "secretary@pm.oy", "USER"); Person user2 = new Person("manager",
+	 * "$2a$10$5V7IGDsCaCx6tFzaz84wWu5StIlDdGyMYaW73MHt59M/3JFE6s1vG",
+	 * "manager@pm.oy", "ADMIN"); Person user3 = new
+	 * Person("boss","$2a$10$IxIjuRzI/k6epKZ9cN6bIuFTAVG1IlyWg8OgvJlL/d3sUQaxEcPyy",
+	 * "boss@pm.oy", "ADMIN"); urepository.save(user1); urepository.save(user2);
+	 * urepository.save(user3);
+	 * 
+	 * log.info("fetch all payments"); for (Payment payment : prepository.findAll())
+	 * { log.info(payment.toString()); }
+	 * 
+	 * }; }
+	 */
 }
